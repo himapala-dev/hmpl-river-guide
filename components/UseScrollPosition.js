fdf97a10cb6ca9e9bb7b01d176c5bab8754512ca
aldhinoerman <aldynoerman@gmail.com>
2021-08-26 10:15:37 +0700

    hook test 1

--- components/UseScrollPosition.js
+++ components/UseScrollPosition.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 const useScrollPosition = () => {
-  if (typeof window === "undefined") return 500;
-
   // Store the state
-  const [scrollPos, setScrollPos] = useState(window.pageYOffset);
+  const [scrollPos, setScrollPos] = useState(0);
 
   // On Scroll
-  const onScroll = () => {
-    setScrollPos(window.pageYOffset);
-  };
-
-  // Add and remove the window listener
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
     };
-  });
 
-  return scrollPos;
-};
+    // just trigger this so that the initial state 
+    // is updated as soon as the component is mounted
+    // related: https://stackoverflow.com/a/63408216
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
 
 export default useScrollPosition;
