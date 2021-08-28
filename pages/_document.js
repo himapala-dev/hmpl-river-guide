3ffc6fd718a56440a64931a5898d257921780046
aldhinoerman <aldynoerman@gmail.com>
2021-08-28 19:50:48 +0700

    move manifest

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang='in'>
+            <Html lang='id'>
                 <Head />
                 <body>
                     <Main />
