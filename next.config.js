b26a784dd6d3254cd71c24699f53e00a22b5c5e6
aldhinoerman <aldynoerman@gmail.com>
2021-08-31 17:20:15 +0700

    try image deploy

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const debug = process.env.NODE_ENV !== "production";
 
 const plugin = withPlugins([
   [optimizedImages,],
-  {
-    basePath: !debug ? '/hmpl-river-guide/' : '',
-  }
 ]);
 
 const nextConfig = {};
@@ -41,6 +38,7 @@ module.exports = {
       "/administrator": { page: "/administrator" },
     }
   },
+  basePath: !debug ? '/hmpl-river-guide' : '',
   assetPrefix: !debug ? '/hmpl-river-guide/' : '',
   webpack: (config, { dev }) => {
     config.module.rules = config.module.rules.map(rule => {
