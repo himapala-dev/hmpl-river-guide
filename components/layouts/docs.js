056ba439b84d9ac64d10cac99848799c18d3565d
aldhinoerman <aldynoerman@gmail.com>
2021-09-02 16:11:02 +0700

    initial about

--- components/layouts/docs.js
+++ components/layouts/docs.js
@@ -2,13 +2,14 @@ import Footer from "../Footer"
 import Meta from "../Meta"
 import Navbar from "../Navbar"
 import SideNav from "../SideNav"
+import styles from "../../styles/DocsLayout.module.scss"
 
 const Documentation = (props) => {
     return (
         <>
             <Meta />
             <Navbar />
-            <div>
+            <div id="docsLayout" className="grid-col two">
                 <SideNav />
                 {props.children}
             </div>
